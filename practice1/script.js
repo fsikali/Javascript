98c23fd00b4ff32c9e32ec54edfaa9cde2f9eba5
fsikali <flemmingsikali499@gmail.com>
2024-05-17 20:43:29 +0300

    Modified: README.md
    Created: 9 new files

---
+++ practice1/script.js
@@ -0,0 +1,89 @@
+// Practice1 js code
+function practice1() {
+    document.getElementById('test').innerHTML= "Maseno School";  
+    document.getElementById('test').style.fontSize = "35px";    
+} 
+
+// Practice2  js code
+function practice2() { 
+    document.getElementById('test').innerHTML= "10";
+}
+
+// Practice3 js code 
+function practice3() { 
+    document.getElementById('test').innerHTML = 2 + 2;
+} 
+function practice03() { 
+    document.getElementById('test1').innerHTML = 2 * 5;
+}
+
+// Practice4 js code 
+function practice4() { 
+    document.write(5 + 6);
+} 
+
+// Practice5 js code 
+function practice5() { 
+    window.alert("My name is Flemming Sikali");
+} 
+function practice05() { 
+    alert(10 + 10);
+}
+
+// Practice6 js code 
+function practice6() { 
+    console.log(10 + 15);
+}
+
+// Practice7 js code 
+function practice7() { 
+    window.print();
+}
+
+/* Practice8_fuctions js code  
+
+let c = addNumbers(10, 15); 
+document.getElementById('test1').innerHTML = c;
+
+function addNumbers(a, b) {
+    return a + b;
+}
+ 
+// Practice8_functions converting fahrenheit to celsius
+let z = toCelsius(77); 
+document.getElementById('test2').innerHTML = "The temprature is " + z + " degrees";
+
+function toCelsius(fahrenheit) { 
+    return 5/9 * (fahrenheit-32);
+}
+
+
+
+// Practice9_object_properties js code  
+function objectCar() {
+
+    const car = {
+        model: "500", 
+        color: "navyblue", 
+        type: "Mercedez", 
+        price: "15million"
+        };
+        
+        document.getElementById('test').innerHTML = car.model;    
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
