96d5c36da813524afc957d2619d82bbd4d4c3d2f
fsikali <flemmingsikali499@gmail.com>
2024-05-21 09:57:15 +0300

    Modified: practice10_event_handlers, files: test.html/test.js

--- practice10_event_handlers/test.js
+++ practice10_event_handlers/test.js
@@ -1,11 +1,15 @@
 // Event handlers js code example 1
+// Event attribute onclick calling function displayDate()
 
 function displayDate() { 
     document.getElementById('test2').innerHTML = Date();
 } 
 
-// Event handlers js code example 2 
+// Event handlers changing the content of its own element example 2 
 
+function changeContent() { 
+    document.getElementById('test4').innerHTML = Date();
+}
 
 
 
